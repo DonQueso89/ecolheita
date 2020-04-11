6e87b9c82492db7abd5461b40329e1f5845701a8
Kees van Ekeren <kees.van.ekeren@mobpro.com>
2020-04-11 22:15:17 +0200

    ListView facelift

--- ecolheita_mobile/src/components/HomeView.jsx
+++ ecolheita_mobile/src/components/HomeView.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, TextInput, ScrollView, View, FlatList, TouchableOpacity } from 'react-native'
-import { SearchBar } from 'react-native-elements';
+import { SearchBar, ListItem, Image, Badge, Icon } from 'react-native-elements';
 import { connect } from "react-redux"
 import { add as addVendors, reset as resetVendors, setQuery as setVendorQuery  } from "../features/vendors/vendorSlice.js"
+
+import { Ionicons } from '@expo/vector-icons'
 
 
 function Search(props) {
@@ -22,11 +24,36 @@ function Offer(props) {
     return <TouchableOpacity style={props.style}><Text>{props.name}</Text></TouchableOpacity>
 }
 
+
+function getNumLeftBadge(props) {
+    return { value: "Sobram " + props.numLeft, status: (props.numLeft < 3 ? "error" : props.numLeft < 5 ? "warning" : "success"), bottom: -20 }
+}
+
+
 function Vendor(props) {
     /*
      * name logo
      */
-    return <TouchableOpacity style={props.style}><Text>{"Comercio " + props.name}</Text></TouchableOpacity>
+    return <ListItem 
+                rightAvatar={{ source: require("../../assets/favicon-32x32.png")}} 
+                bottomDivider 
+                topDivider 
+                style={props.style}
+                title={<Text>{props.name}    <Ionicons name="md-clock"/>{props.openFrom + "/" + props.openUntil}</Text>}
+                subtitle={
+                    <View style={styles.vendorBackground}>
+                        <Image 
+                            source={require("../../assets/foodbg.jpg")}
+                            style={{height:80, width: 300, borderRadius: 5, resizeMode: "stretch" }}>
+
+                            <Badge value={props.distance + " metros"} containerStyle={{bottom: -30, left: -110}}/>
+                            <Badge value={ "R$" + props.price} containerStyle={{bottom: -30, left: -125}}/>
+                        </Image>
+                    </View>
+                }  
+                chevron={true} 
+                badge={getNumLeftBadge(props)}
+        />
 }
 
 function HomeView(props) {
@@ -35,8 +62,8 @@ function HomeView(props) {
     <Search style={{backgroundColor: '#fa5'}} {...props} />
       <View style={styles.container}>
     <FlatList
-      data={props.vendorQuery.length ? props.vendors.filter(x => x.toLowerCase().includes(props.vendorQuery.toLowerCase())) : props.vendors}
-      renderItem={({item}) => <Vendor style={styles.navButton} name={item}  />} />
+      data={props.vendorQuery.length ? props.vendors.filter(x => x.name.toLowerCase().includes(props.vendorQuery.toLowerCase())) : props.vendors}
+      renderItem={({item}) => <Vendor style={styles.vendor} {...item}  />} />
       </View>
       </>
   )
@@ -89,11 +116,11 @@ const styles = StyleSheet.create({
   },
   vendor: {
     backgroundColor: '#a50',
-    marginVertical: 5,
-    marginHorizontal: 5,
+  },
+  vendorBackground: {
+    flexDirection: "row",
+    width: 390,
+    height: 80,
     borderRadius: 5,
-    padding: 20,
-    borderColor: "red",
-    height: 80
   }
 })
