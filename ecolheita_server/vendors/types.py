efcb2c82027ebe1091e8ddfac550140e66bb3726
Kees van Ekeren <kees.van.ekeren@mobpro.com>
2020-05-10 22:53:37 +0200

    complete offer and vendor type for mockup

--- ecolheita_server/vendors/types.py
+++ ecolheita_server/vendors/types.py
@@ -1,21 +1,33 @@
 from graphene_django import DjangoObjectType
+from graphene_django.fields import DjangoConnectionField
 from vendors.models import Vendor, Offer, Organization
 from core.types import UUIDNode
-
-
-class VendorNode(DjangoObjectType):
-    class Meta:
-        model = Vendor
-        interfaces = (UUIDNode,)
-        filter_fields = ("name",)
 
 
 class OfferNode(DjangoObjectType):
     class Meta:
         model = Offer
         interfaces = (UUIDNode,)
-        fields = ("name", "status", "offer", "quantity", "collected_at")
+        fields = ("name", "vendor", "num_left", "collect_until", "collect_from", "unit_price")
         filter_fields = ("name",)
+    
+    @staticmethod
+    def resolve_unit_price(root, info):
+        return root.unit_price.amount
+
+class VendorNode(DjangoObjectType):
+    offers = DjangoConnectionField(OfferNode)
+
+    class Meta:
+        model = Vendor
+        interfaces = (UUIDNode,)
+        filter_fields = ("name",)
+        fields = ("name", "organization", "open_from", "open_until", "website", "logo", "default_backdrop")
+
+    @staticmethod
+    def resolve_offers(root, info):
+        return root.offer_set.all()
+
 
 
 class OrganizationNode(DjangoObjectType):
